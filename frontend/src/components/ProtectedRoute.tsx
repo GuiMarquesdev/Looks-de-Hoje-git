265e41c71e82d65259874586a17c935ad7793b6a
Guilherme <guimarquesbrito@gmail.com>
2025-11-12 14:25:27 -0300

    feat: Adiciona componente de gestão de Hero

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../contexts/AuthContext";
 
 /**
  * Componente que verifica a autenticação antes de renderizar as rotas filhas.
